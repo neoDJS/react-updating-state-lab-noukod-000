d68ab458aff0d9ea08e4f372bfef37c26c72ed3f
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-07 19:20:03 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -3,5 +3,17 @@ import React from 'react';
 
 
 export default class DigitalClicker extends React.Component {
+  constructor(){
+    super()
 
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  render(){
+    return (
+      <button>timesClicked</button>
+    );
+  }
 }
